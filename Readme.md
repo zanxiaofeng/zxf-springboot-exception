87052dd643cae464205d07aa0bfc52f517febf7d
davis <zanxiaofeng@163.com>
2022-05-26 21:03:27 +0800

    update

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
 # 如何隐式的将数据传递给RequestInterceptor
-- By Global 利用全局变量
+- By Global 利用全局变量，配置文件
 - By Thread 利用ThreadLocal, 如Spring的RequestContextHolder，SecurityContextＨolder
 - By Request　利用@RequestHeader("X-Temp-Parameter-abc") 或　＠RequestParameter("x-temp-parameter-abc") 
--- service-b/src/main/java/zxf/springboot/service/b/support/feign/ServiceARequestInterceptor.java
+++ service-b/src/main/java/zxf/springboot/service/b/support/feign/ServiceARequestInterceptor.java
@@ -1,19 +1,20 @@
 package zxf.springboot.service.b.support.feign;
 
-
+import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import feign.auth.BasicAuthRequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import zxf.springboot.support.Spring.ApplicationContextProvider;
 
 @Slf4j
-public class ServiceARequestInterceptor extends BasicAuthRequestInterceptor {
-    public ServiceARequestInterceptor() {
-        super("davis", "davis");
-    }
-
+public class ServiceARequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
-        super.apply(template);
+        ServiceAProperties serviceAProperties = ApplicationContextProvider.getApplicationContext()
+                .getBean(ServiceAProperties.class);
+        BasicAuthRequestInterceptor basicAuthRequestInterceptor = new BasicAuthRequestInterceptor(serviceAProperties.getUsername(),
+                serviceAProperties.getPassword());
+        basicAuthRequestInterceptor.apply(template);
         log.info("ServiceARequestInterceptor::apply X-AGE {}", template.headers().get("X-AGE"));
     }
 }
